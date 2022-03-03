1e0e7df984bd6765f08bfb4f7b78a369b5a27918
CristianoMorais <upcristiano@gmail.com>
2022-03-03 20:01:34 -0300

    Primeiro

---
+++ script.js
@@ -0,0 +1,32 @@
+
+let totalSlides = document.querySelectorAll('.slider--item--image').length;
+let sliderWidth = document.querySelector('.slider').clientWidth;
+let currentSlide = 0;
+   
+document.querySelector('.slider--width').style.width = 
+    `${sliderWidth * totalSlides}px`;
+document.querySelector('.slider--controls').style.width = 
+    `${sliderWidth}px`;
+document.querySelector('.slider--controls').style.height = 
+    `${document.querySelector('.slider').clientHeight}px`;
+function goPrev() {
+    currentSlide--;
+    if(currentSlide < 0){
+        currentSlide = totalSlides - 5;
+    }
+    updateMargin();
+}
+function goNext() {
+    currentSlide++;
+    if(currentSlide > (totalSlides - 5)) {
+        currentSlide = 0;
+    }
+    updateMargin();
+}
+function updateMargin() {
+    let sliderItemWidth = document.querySelector('.slider--item').clientWidth;
+    let newMargin = (currentSlide * sliderItemWidth);
+    document.querySelector('.slider--width').style.marginLeft = 
+    `-${newMargin}px`;
+}
+setInterval(goNext, 2500);
